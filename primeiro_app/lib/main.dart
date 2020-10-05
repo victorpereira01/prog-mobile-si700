5bf4cb397b33766d1083c1fb4a8db134a2b548f5
Alexandre Queiroz <xandemon@gmail.com>
2020-10-04 23:45:02 -0300

    Coloquei minha parte
    Adicionei minhas informações no main.dart e coloquei minha foto na pasta assets

--- primeiro_app/lib/main.dart
+++ primeiro_app/lib/main.dart
@@ -36,7 +36,7 @@ class MyFirstApp extends StatelessWidget {
         ),
         body: TabBarView(children: [
           Center(child: generateVictorView()),
-          Center(child: Text("TELA 2")),
+          Center(child: generateAlexandreView()),
           Center(child: generateTrabalhoView()),
         ]));
   }
@@ -82,6 +82,48 @@ Widget generateVictorView() {
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
                   color: Colors.deepPurple)))
+    ],
+  ));
+}
+
+Widget generateAlexandreView() {
+  return Center(
+      child: Column(
+    children: [
+      Container(
+        margin: EdgeInsets.only(top: 15),
+        child: Text(
+          "Alexandre Queiroz - 212344",
+          style: TextStyle(
+              fontSize: 23, fontWeight: FontWeight.bold, color: Colors.indigo),
+        ),
+      ),
+      Text(
+        "TADS - 6º semestre",
+        style: TextStyle(
+            fontSize: 20, fontWeight: FontWeight.bold, color: Colors.indigo),
+      ),
+      Container(
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: Colors.indigo, width: 5)),
+          margin: EdgeInsets.only(right: 80, left: 80, top: 10),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(200),
+              child: Image.asset('assets/images/alexandre.jpg'))),
+      Padding(
+          padding: EdgeInsets.all(20),
+          child: Text(
+              "No momento não estou estágiando nem trabalhando, mas minha principal área de "
+              "atuação é a de Suporte que foi a área que eu estagiei para concluir meu técnico. "
+              "Eu não sou o maior fã de programação, mas procuro saber um pouco sobre diversas "
+              "linguagens, para o caso de eu eventualmente ter uma ideia que eu mesmo queira "
+              "transformar em um software. No meu tempo livre, o que eu mais gosto de fazer é "
+              "jogar, ouvir música e antes da quarentena, gostava de caminhar.",
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.indigo)))
     ],
   ));
 }
